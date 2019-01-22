7802dc1b175b01c73304565b2c2cdc6656469c42
Gogs <gogs@fake.local>
2019-01-22 15:35:31 +0200

    Initial Commit

---
+++ UserRegistrationSystem/src/main/java/com/atos/boyan/UserRegistrationSystemApplication.java
@@ -0,0 +1,14 @@
+package com.atos.boyan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserRegistrationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserRegistrationSystemApplication.class, args);
+	}
+
+}
+
