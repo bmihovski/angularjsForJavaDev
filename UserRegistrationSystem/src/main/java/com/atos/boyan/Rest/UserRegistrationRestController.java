4cb4c591c2109776ec3e61f7f8fd648a16ae9cc7
Gogs <gogs@fake.local>
2019-02-22 18:15:47 +0200

    Squashed commit of the following:
    commit 957492a723adc83b3dc572966f8346bb0833ca81
    Author: Gogs <gogs@fake.local>
    Date:   Fri Feb 22 18:15:16 2019 +0200
    
    	Spring security implemented at http and method level users are taken
    form db
    
    commit 8b027d6ae2a6092cbb9720e1db47570140836bef
    Author: Gogs <gogs@fake.local>
    Date:   Thu Feb 21 17:45:12 2019 +0200
    
    	In memory and config file security implemented
    
    commit ca71661d598b0b4df5c740cbf0474f616670001a
    Author: Gogs <gogs@fake.local>
    Date:   Thu Feb 21 16:58:27 2019 +0200
    
    	Spring security added

--- UserRegistrationSystem/src/main/java/com/atos/boyan/Rest/UserRegistrationRestController.java
+++ UserRegistrationSystem/src/main/java/com/atos/boyan/Rest/UserRegistrationRestController.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -93,6 +94,7 @@ public class UserRegistrationRestController {
 	}
 
 	@DeleteMapping("/{id}")
+	@PreAuthorize("hasAuthority('ADMIN')")
 	public ResponseEntity<UsersDTO> deleteUser(@PathVariable("id") final Long id) {
 		UsersDTO user = userJpaRepository.findById(id);
 		if (user == null) {
