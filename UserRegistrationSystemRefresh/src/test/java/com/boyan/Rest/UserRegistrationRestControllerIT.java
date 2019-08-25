2302ba7d64de60bfe40e0ad0f840d579564cb2cc
bmihovski <bmihovski@live.com>
2019-08-25 14:11:37 +0300

    Asciidoc example completed

--- UserRegistrationSystemRefresh/src/test/java/com/boyan/Rest/UserRegistrationRestControllerIT.java
+++ UserRegistrationSystemRefresh/src/test/java/com/boyan/Rest/UserRegistrationRestControllerIT.java
@@ -3,7 +3,12 @@ package com.boyan.Rest;
 import static org.hamcrest.CoreMatchers.is;
 import static org.mockito.Mockito.when;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessRequest;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -37,37 +42,40 @@ public class UserRegistrationRestControllerIT {
 
 	@Autowired
 	MockMvc mockMvc;
-	
+
 	@MockBean
 	UserJpaRepository userJpaRepositoryMock;
-	
+
 	private MediaType contentType;
 	private UsersDTO user;
-	
+
+
 	@Before
 	public void setUp() {
-		contentType = new MediaType(MediaType.APPLICATION_JSON.getType(),
-				MediaType.APPLICATION_JSON.getSubtype(),
+		contentType = new MediaType(MediaType.APPLICATION_JSON.getType(), MediaType.APPLICATION_JSON.getSubtype(),
 				Charset.forName("utf-8"));
 		user = new UsersDTO();
 		user.setName("Icho");
 		user.setAddress("opa str");
 		user.setEmail("test@test.com");
 	}
-	
+
 	@Test
 	@WithMockUser(username = "user")
 	public void shouldReturnSuccessMessage() throws Exception {
+
 		when(this.userJpaRepositoryMock.findById(1L)).thenReturn(Optional.of(user));
-		
-		this.mockMvc.perform(get("/api/user/1"))
-			.andExpect(status().isOk())
-			.andExpect(content().contentType(contentType))
-			.andExpect(jsonPath("$.name", is("Icho")))
-			.andExpect(jsonPath("$.address", is("opa str")))
-			.andExpect(jsonPath("$.email", is("test@test.com")))
-			.andDo(document("user"))
-			.andDo(print());
+
+		this.mockMvc.perform(get("/api/user/1")).andExpect(status().isOk())
+				.andExpect(content().contentType(contentType)).andExpect(jsonPath("$.name", is("Icho")))
+				.andExpect(jsonPath("$.address", is("opa str"))).andExpect(jsonPath("$.email", is("test@test.com")))
+				.andDo(document("{ClassName}/{methodName}", preprocessRequest(prettyPrint()),
+						preprocessResponse(prettyPrint()),
+						responseFields(fieldWithPath("id").description("User Identifier"),
+								fieldWithPath("name").description("User Name"),
+								fieldWithPath("address").description("User Address"),
+								fieldWithPath("email").description("User e-mail"))))
+				.andDo(print());
 	}
 
 }
